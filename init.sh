315a545bfc7486e3387323515a59cef2ae255912
Heriberto Madrigal <3331780+magic-madrigal@users.noreply.github.com>
2019-12-09 23:51:53 -0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -37,6 +37,7 @@ brewCaskApps=(
   font-fira-code
   font-roboto-mono
   google-chrome
+  grammarly
   iterm2
   keybase
   kitematic
@@ -551,7 +552,8 @@ main() {
       defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Slack.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
       defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/iTerm.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
       defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Spotify.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
-      defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/rekordbox 5/rekordbox.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
+      defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>"/Applications/rekordbox 5/rekordbox.app"</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
+      defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Grammarly.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
       killall Dock
       defaults write com.apple.systemuiserver menuExtras -array-add "/System/Library/CoreServices/Menu Extras/Bluetooth.menu"      
       defaults write com.apple.systemuiserver menuExtras -array-add "/System/Library/CoreServices/Menu Extras/Volume.menu"
