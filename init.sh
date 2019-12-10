bc916cd6de6ef1842ce3225f6dad06136d0eb9a9
Heriberto Madrigal <3331780+magic-madrigal@users.noreply.github.com>
2019-12-09 22:01:44 -0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -552,8 +552,8 @@ main() {
       defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/iTerm.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
       defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Spotify.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
       killall Dock
-      defaults write com.apple.systemuiserver menuExtras -array "/System/Library/CoreServices/Menu Extras/Volume.menu"
-      defaults write com.apple.systemuiserver menuExtras -array "/System/Library/CoreServices/Menu Extras/Bluetooth.menu"
+      defaults write com.apple.systemuiserver menuExtras -array-add "/System/Library/CoreServices/Menu Extras/Bluetooth.menu"      
+      defaults write com.apple.systemuiserver menuExtras -array-add "/System/Library/CoreServices/Menu Extras/Volume.menu"
       defaults write com.apple.menuextra.battery ShowPercent YES
       defaults write com.apple.menuextra.clock DateFormat -string "EEE MMM d  h:mm a"
       killall SystemUIServer
